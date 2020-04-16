6ca5bf799cb504fce5ab5a3deb7b200aae70f1fe
lihongwei <lihongwei549@163.com>
2020-04-16 16:20:16 +0800

    up

--- src/components/UploadVmTpl.vue
+++ src/components/UploadVmTpl.vue
@@ -70,7 +70,7 @@
                 将文件拖到此处，或
                 <em>点击上传</em>
               </div>
-              <div class="el-upload__tip" slot="tip">只能上传iso文件，且不超过5G</div>
+              <div class="el-upload__tip" slot="tip">只能上传虚拟机模板文件</div>
             </el-upload>
           </el-form-item>
           <el-form-item>
